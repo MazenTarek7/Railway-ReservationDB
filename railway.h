0c26de215ed19be8d9d77bc47dfb7c772833cca1
MazenTarek7 <56880548+MazenTarek7@users.noreply.github.com>
2020-10-29 13:41:32 +0200

    Add files via upload
    Header file including structs

---
+++ railway.h
@@ -0,0 +1,15 @@
+struct bookTicket
+{
+    char fname[10];
+    char lname[10];
+    int trainNum;
+    int num_seats;
+    char phone[11];
+};
+struct addTrain
+{
+    int trainNum;
+    char trainName[30];
+    char destination[50];
+    int price;
+};
